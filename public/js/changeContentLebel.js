5185ac5025350976953e26551f92f10e48a89109
Lucas Encrenaz <lucas.encrenaz3@gmail.com>
2021-05-01 17:36:19 +0200

    Lucas E -- Change eng in fr

---
+++ public/js/changeContentLebel.js
@@ -0,0 +1,3 @@
+$(".articlesAccount .accountUsername .accountFormChangePseudo .col-form-label").text("Pseudo");
+$(".contentCreate .form-group label[for='post_title']").text("Titre du post");
+$(".contentCreate .form-group label[for='post_content']").text("Contenu du post");
